32a553bb70d7e347ab0ae4e6dbf05369bc05509e
davy <davydesouzabar@gmail.com>
2023-10-22 18:41:56 -0400

    chore: config application theme

---
+++ src/presentation/styles/theme.ts
@@ -0,0 +1,30 @@
+export const theme = {
+  colors: {
+    primary: "#6A5AE0",
+    secondary: "#9087E5",
+    tertiary: "#FF8FA2",
+
+    support: {
+      "accent-1": "#FFD6DD",
+      "accent-2": "#C9F2E9",
+      "accent-3": "#C4D0FB",
+    },
+
+    neutral: {
+      black: "#0C092A",
+      white: "#FFF",
+
+      "gey-1": "#49465F",
+      "gey-2": "#858494",
+      "gey-3": "#CCC",
+      "gey-4": "#E6E6E6",
+      "gey-5": "#EFEEFC",
+    },
+
+    fonts: {
+      bold: "Rubik_700Bold",
+      medium: "Rubik_500Medium",
+      regular: "Rubik_400Regular",
+    },
+  },
+};
