6f5f430de432fe52dd441fccac5197487b87ffbe
davy <davydesouzabar@gmail.com>
2023-10-22 23:35:10 -0400

    test(button): test button variants

--- src/presentation/screens/home/home.tsx
+++ src/presentation/screens/home/home.tsx
@@ -1,13 +1,13 @@
 import { View } from "react-native";
 import React from "react";
-import { Text } from "@presentation/components";
+import { Button } from "@presentation/components";
 
 export const Home = (): ReturnType<React.FC> => {
   return (
-    <View>
-      <Text.Root variant="heading-1" color="primary">
-        Hello World
-      </Text.Root>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button.Root variant="primary">
+        <Button.Text>Button</Button.Text>
+      </Button.Root>
     </View>
   );
 };
