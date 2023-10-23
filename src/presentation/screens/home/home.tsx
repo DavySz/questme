688adfa72d589dad4551add43d52d5c5b0e2cc56
davy <davydesouzabar@gmail.com>
2023-10-23 00:15:52 -0400

    test(button): facebook and google variants

--- src/presentation/screens/home/home.tsx
+++ src/presentation/screens/home/home.tsx
@@ -1,12 +1,21 @@
 import { View } from "react-native";
 import React from "react";
 import { Button } from "@presentation/components";
+import GoogleIcon from "@presentation/assets/google-icon.svg";
 
 export const Home = (): ReturnType<React.FC> => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button.Root variant="primary">
-        <Button.Text>Button</Button.Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 24,
+      }}
+    >
+      <Button.Root variant="google">
+        <Button.Icon icon={GoogleIcon} side="left" />
+        <Button.Text>Login with Google</Button.Text>
       </Button.Root>
     </View>
   );
