f0a8689ca8c013c95e1d0efc2ce125c6cbdfbfa3
davy <davydesouzabar@gmail.com>
2023-10-22 20:19:25 -0400

    feat: test text component

--- src/presentation/screens/home/home.tsx
+++ src/presentation/screens/home/home.tsx
@@ -1,10 +1,13 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import React from "react";
+import { Text } from "@presentation/components";
 
 export const Home = (): ReturnType<React.FC> => {
   return (
     <View>
-      <Text>Hello World</Text>
+      <Text.Root variant="heading-1" color="primary">
+        Hello World
+      </Text.Root>
     </View>
   );
 };
