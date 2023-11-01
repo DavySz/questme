87dcbf6b2ebd86f773cd5992c0f853c5360852cc
davy <davydesouzabar@gmail.com>
2023-10-31 21:13:43 -0400

    test: ensure login button method is called when button is clicked

--- tests/presentation/screens/initial.test.tsx
+++ tests/presentation/screens/initial.test.tsx
@@ -1,9 +1,24 @@
 import { InitialContainer } from "@presentation/screens";
 import { customRender } from "../../utils/custom-render";
+import { fireEvent } from "@testing-library/react-native";
+import { Initial } from "@presentation/screens/initial/initial";
 
 describe("Initial", () => {
   test("Should render without errors", () => {
     const tree = customRender(<InitialContainer />);
     expect(tree).toMatchSnapshot();
+  });
+
+  test("Should call one time mocked method when button is clicked", () => {
+    const handleGoToLoginScreenMock = jest.fn();
+
+    const { getByTestId } = customRender(
+      <Initial handleGoToLoginScreen={handleGoToLoginScreenMock} />
+    );
+
+    const loginButton = getByTestId("button-login");
+    fireEvent.press(loginButton);
+
+    expect(handleGoToLoginScreenMock).toHaveBeenCalledTimes(1);
   });
 });
