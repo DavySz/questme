da08c3f7dfa8126b318de1d8e0a40f6175770719
davy <davydesouzabar@gmail.com>
2023-10-31 22:03:18 -0400

    refactor: use testing public navigator helper

--- tests/presentation/screens/initial.test.tsx
+++ tests/presentation/screens/initial.test.tsx
@@ -1,26 +1,11 @@
 import { InitialContainer } from "@presentation/screens";
-import { customRender } from "../../utils/custom-render";
+import { customRender, TestingPublicNavigator } from "../../utils";
 import { fireEvent, render } from "@testing-library/react-native";
 import { Initial } from "@presentation/screens/initial/initial";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "@presentation/styles";
 
 import React from "react";
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { MakeLoginScreen, MakeInitialScreen } from "@main/factories/screens";
-import { NavigationContainer } from "@react-navigation/native";
-
-const Stack = createNativeStackNavigator();
-
-export const TestingRoutes = () => {
-  return (
-    <Stack.Navigator initialRouteName="initial">
-      <Stack.Screen name="initial" component={MakeInitialScreen} />
-      <Stack.Screen name="login" component={MakeLoginScreen} />
-    </Stack.Navigator>
-  );
-};
 
 describe("Initial", () => {
   test("Should render without errors", () => {
@@ -44,9 +29,7 @@ describe("Initial", () => {
   test("Should navigate to login screen when login button is clicked", async () => {
     const { getByTestId, findByTestId } = render(
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <TestingRoutes />
-        </NavigationContainer>
+        <TestingPublicNavigator currentRoute="initial" />
       </ThemeProvider>
     );
 
