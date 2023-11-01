6c3a0eb0253a9b11c5583e0ba2ba384f68170302
davy <davydesouzabar@gmail.com>
2023-10-31 22:18:54 -0400

    test: ensure go back to the previous screen when go back button is pressed

--- tests/presentation/screens/login.test.tsx
+++ tests/presentation/screens/login.test.tsx
@@ -1,8 +1,22 @@
 import { LoginContainer } from "@presentation/screens";
-import { customRender } from "../../utils/custom-render";
+import { customRender, TestingPublicNavigator } from "../../utils";
+import { fireEvent, render } from "@testing-library/react-native";
 describe("Login", () => {
   test("Should render without errors", () => {
     const tree = customRender(<LoginContainer />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test("Should go back to the previous screen when go back button is pressed", async () => {
+    const { getByTestId, findByTestId } = render(
+      <TestingPublicNavigator currentRoute="login" />
+    );
+
+    const goBackButton = getByTestId("go-back-button");
+    fireEvent.press(goBackButton);
+
+    const loginScreen = await findByTestId("initial-screen-content");
+
+    expect(loginScreen).toBeTruthy();
   });
 });
