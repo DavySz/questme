0db990a12256016d145f04c1827e824b123d8c04
davy <davydesouzabar@gmail.com>
2023-11-04 11:30:35 -0400

    test: ensure navigate to sign up with email when this button is clicked

--- tests/presentation/screens/sign-up.test.tsx
+++ tests/presentation/screens/sign-up.test.tsx
@@ -1,10 +1,10 @@
 import { fireEvent, render } from "@testing-library/react-native";
 import { TestingPublicNavigator, customRender } from "../../utils";
-import { LoginContainer } from "@presentation/screens";
+import { SignUpContainer } from "@presentation/screens";
 
 describe("SignUp", () => {
   test("Should render screen without errors", () => {
-    const tree = customRender(<LoginContainer />).toJSON();
+    const tree = customRender(<SignUpContainer />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
@@ -22,5 +22,20 @@ describe("SignUp", () => {
     const previousScreen = await findByTestId("initial-screen-content");
 
     expect(previousScreen).toBeTruthy();
+  });
+
+  test("Should navigate to sign up with email when this button is clicked", async () => {
+    const { getByTestId, findByTestId } = render(
+      <TestingPublicNavigator currentRoute="sign-up" />
+    );
+
+    const buttonSignUpWithEmail = getByTestId("button-sign-up-with-email");
+    fireEvent.press(buttonSignUpWithEmail);
+
+    const signUpWithEmailFirstStepScreen = await findByTestId(
+      "sign-up-email-first-step-screen-content"
+    );
+
+    expect(signUpWithEmailFirstStepScreen).toBeTruthy();
   });
 });
