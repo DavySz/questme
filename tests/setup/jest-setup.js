9990e9e6cfb8c8a5c6be93090afc2949d57027b5
davy <davydesouzabar@gmail.com>
2023-11-07 18:34:44 -0400

    chore: config react native pie chart

--- tests/setup/jest-setup.js
+++ tests/setup/jest-setup.js
@@ -1,5 +1,8 @@
 // include this line for mocking react-native-gesture-handler
 import "react-native-gesture-handler/jestSetup";
+import "react-native-pie-chart";
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
+jest.mock("react-native-pie-chart", () => ({}));
