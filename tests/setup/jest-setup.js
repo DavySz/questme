9be77dcba61d785ec3672f6e5dfae034a236d795
davy <davydesouzabar@gmail.com>
2023-11-08 14:41:06 -0400

    fix: react-native-pie-chart mock

--- tests/setup/jest-setup.js
+++ tests/setup/jest-setup.js
@@ -5,4 +5,6 @@ import "react-native-pie-chart";
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
-jest.mock("react-native-pie-chart", () => ({}));
+jest.mock("react-native-pie-chart", () => {
+  return jest.fn();
+});
