32ab55aab818525ba6639d8887f3ce0eb722899b
davy <davydesouzabar@gmail.com>
2023-11-13 16:00:29 -0400

    chore: config firebase files

--- tests/setup/jest-setup.js
+++ tests/setup/jest-setup.js
@@ -1,6 +1,8 @@
 // include this line for mocking react-native-gesture-handler
 import "react-native-gesture-handler/jestSetup";
 import "react-native-pie-chart";
+import "firebase/firestore";
+import "firebase/app";
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
@@ -8,3 +10,13 @@ jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 jest.mock("react-native-pie-chart", () => {
   return jest.fn();
 });
+
+jest.mock("firebase/firestore", () => ({
+  addDoc: jest.fn(),
+  collection: jest.fn(),
+  getFirestore: jest.fn(),
+}));
+
+jest.mock("firebase/app", () => ({
+  initializeApp: jest.fn(),
+}));
