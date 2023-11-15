f3b3c754e90b1289d3529a000d20cf89c4a9b5af
davy <davydesouzabar@gmail.com>
2023-11-15 01:11:55 -0400

    test: ensure eturn true when the number of characters is greater than or equal to 8

--- tests/validation/fields/password-validator.test.ts
+++ tests/validation/fields/password-validator.test.ts
@@ -14,4 +14,16 @@ describe("PasswordValidator", () => {
     const isValid = sut.isValid("invalid");
     expect(isValid).toBeFalsy();
   });
+
+  test("Should return true when the number of characters is equal to 8", () => {
+    const { sut } = makeSut();
+    const isValid = sut.isValid("validPas");
+    expect(isValid).toBeTruthy();
+  });
+
+  test("Should return true when the number of characters is greater than 8", () => {
+    const { sut } = makeSut();
+    const isValid = sut.isValid("valid-password");
+    expect(isValid).toBeTruthy();
+  });
 });
