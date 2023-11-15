7e7a4e89be3a085b99bf0addeab2458950086b6c
davy <davydesouzabar@gmail.com>
2023-11-15 00:47:17 -0400

    feat: using usecase factory to pass method to screen

--- src/main/factories/screens/sign-up-email-third-step.factory.tsx
+++ src/main/factories/screens/sign-up-email-third-step.factory.tsx
@@ -1,6 +1,11 @@
 import { SignUpEmailThirdStepContainer } from "@presentation/screens";
+import { makeCreateUserByEmailPassword } from "../usecases";
 import { ReactNode } from "react";
 
 export function MakeSignUpEmailThirdStepScreen(): ReactNode {
-  return <SignUpEmailThirdStepContainer />;
+  return (
+    <SignUpEmailThirdStepContainer
+      createUserByEmailPassword={makeCreateUserByEmailPassword()}
+    />
+  );
 }
