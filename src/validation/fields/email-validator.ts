5a19b6f5f785a16cde6742bee5e4697ce4efa2e0
davy <davydesouzabar@gmail.com>
2023-11-15 00:48:58 -0400

    feat: create email validator

---
+++ src/validation/fields/email-validator.ts
@@ -0,0 +1,10 @@
+import { FieldValidatorProtocol } from "../protocols";
+
+export class EmailValidator implements FieldValidatorProtocol {
+  isValid(value: string): boolean {
+    const emailRegex =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+    return emailRegex.test(value);
+  }
+}
