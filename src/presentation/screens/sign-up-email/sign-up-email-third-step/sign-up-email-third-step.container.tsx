470706ad7e0572811e0c0f7a3180d686705477b8
davy <davydesouzabar@gmail.com>
2023-12-19 22:35:32 -0400

    feat(home): implement create user feature

--- src/presentation/screens/sign-up-email/sign-up-email-third-step/sign-up-email-third-step.container.tsx
+++ src/presentation/screens/sign-up-email/sign-up-email-third-step/sign-up-email-third-step.container.tsx
@@ -4,10 +4,14 @@ import { useNavigation } from "@react-navigation/native";
 import { signUpEmailState } from "@presentation/recoil/atoms";
 
 import { SignUpEmailThirdStepUI } from "./sign-up-email-third-step.ui";
+import { ISignUpEmailThirdStepContainer } from "./types";
 
-export const SignUpEmailThirdStepContainer = (): ReturnType<React.FC> => {
+export const SignUpEmailThirdStepContainer = ({
+  createUserByEmailPassword,
+}: ISignUpEmailThirdStepContainer): ReturnType<
+  React.FC<ISignUpEmailThirdStepContainer>
+> => {
   const [user, setUser] = useRecoilState(signUpEmailState);
-
   const navigation = useNavigation();
 
   const handleGoBack = () => {
@@ -18,11 +22,20 @@ export const SignUpEmailThirdStepContainer = (): ReturnType<React.FC> => {
     setUser((prev) => ({ ...prev, username }));
   };
 
+  const handleCreateUser = async () => {
+    try {
+      await createUserByEmailPassword.create(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <SignUpEmailThirdStepUI
       user={user}
       handleGoBack={handleGoBack}
       handleUpdateUser={handleUpdateUser}
+      handleCreateUser={handleCreateUser}
     />
   );
 };
